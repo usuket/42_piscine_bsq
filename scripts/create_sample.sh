0f70d3a74d0a10ebb9889e1115088406617bdb23
usuket <sy0202002@gmail.com>
2017-02-01 17:23:12 -0800

    add create_sample

--- scripts/create_sample.sh
+++ scripts/create_sample.sh
@@ -12,8 +12,8 @@
 # **************************************************************************** #
 
 echo "CREATE MAP START for ../input/"
-time perl map_maker.pl 1 1 1 > ../input/map1_1_1
-time perl map_maker.pl 2 2 0 > ../input/map2_2_0
+#time perl map_maker.pl 1 1 1 > ../input/map1_1_1
+#time perl map_maker.pl 2 2 0 > ../input/map2_2_0
 #time perl map_maker.pl 3 3 1 > ../input/map3_3_1
 #time perl map_maker.pl 5 5 2 > ../input/map5_5_2
 #time perl map_maker.pl 7 8 2 > ../input/map7_8_2
@@ -24,6 +24,11 @@ time perl map_maker.pl 2 2 0 > ../input/map2_2_0
 #time perl map_maker.pl 500 500 2 > ../input/map500_500_2
 #time perl map_maker.pl 1000 1000 2 > ../input/map1000_1000_2
 #time perl map_maker.pl 3000 2000 2 > ../input/map3000_2000_2
-time perl map_maker.pl 5000 5000 2 > ../input/map5000_5000_2
+#time perl map_maker.pl 5000 5000 2 > ../input/map5000_5000_2
+#time perl map_maker.pl 6000 6000 2 > ../input/map6000_6000_2
+#time perl map_maker.pl 7000 7000 2 > ../input/map7000_7000_2
+#time perl map_maker.pl 8000 8000 2 > ../input/map8000_8000_2
+#time perl map_maker.pl 9000 9000 2 > ../input/map9000_9000_2
 #time perl map_maker.pl 10000 10000 2 > ../input/map10000_10000_2
+#time perl map_maker.pl 10000 10000 100 > ../input/map10000_10000_100
 echo "CREATE MAP DONE"
